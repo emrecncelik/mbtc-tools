fe3851caadc532eca2f7979e99b60c60b381af25
emrecncelik <emrecncelik@gmail.com>
2022-03-27 00:00:36 +0300

    add run script

---
+++ scripts/run_data_formatting.sh
@@ -0,0 +1,7 @@
+python run_data_formatting.py \
+    --mst_dir /home/emrecan/workspace/psychology-project/data/MST_all \
+    --mst_var_file /home/emrecan/workspace/psychology-project/data/mst_variables.csv \
+    --attachment_dir /home/emrecan/workspace/psychology-project/data/Attachment \
+    --behavior_dir "/home/emrecan/workspace/psychology-project/data/Behavior Problems" \
+    --output_file "/home/emrecan/workspace/psychology-project/data/mst_formatted.csv" \
+    --sep "<#>"
