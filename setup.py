2b20fe98d5f575eff3e16e76f9dbd27b34cd767b
emrecncelik <emrecncelik@gmail.com>
2022-03-26 23:23:07 +0300

    add package stuff

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt") as requirements_file:
+    requirements = requirements_file.readlines()
+
+
+setup(
+    name="mbtc_tools",
+    version="0.0.1",
+    description="Automated NLP tools for MBT-C.",
+    python_requires=">=3.8",
+    author="emrecncelik",
+    author_email="emrecncelik@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[requirements],
+    long_description=readme,
+    include_package_data=True,
+    packages=find_packages(
+        include=[
+            "mbtc_tools.*",
+        ]
+    ),
+)
