6a742fdbe360469f8c2e7bf2b800d2c3c22ea9f1
emrecncelik <emrecncelik@gmail.com>
2022-04-05 23:13:46 +0300

    add train test split, target var. selection, cleaning

--- scripts/format_data.py
+++ scripts/format_data.py
@@ -1,9 +1,13 @@
+from __future__ import annotations
+
+import os
 import logging
 import pandas as pd
 from typing import Optional
 from dataclasses import dataclass
-from simple_parsing import ArgumentParser
+from simple_parsing import ArgumentParser, field
 from mbtc_tools.data_formatting import DataFormatter
+from sklearn.model_selection import train_test_split
 
 logging.basicConfig(level=logging.INFO)
 
@@ -14,8 +18,39 @@ class Configuration:
     mst_var_file: str
     attachment_dir: str
     behavior_dir: str
-    output_file: Optional[str] = "mst_formatted.csv"
+    target_variable: str = field(
+        choices=[
+            "Gen",
+            "Age",
+            "CB_Tot_R",
+            "SSC_Tot",
+            "attachment_label",
+            "behavior_problems_label",
+        ],
+    )
+    target_variable_type: str = field(choices=["categorical", "numeric"])
+    test_size: float = 0.15
+    min_label_frequency: int = 3
+    output_dir: Optional[str] = "mst_formatted"
     sep: Optional[str] = "<#>"
+    columns: list[str] = field(
+        default_factory=[
+            "filename",
+            "therapist_sent",
+            "child_sent",
+            "conversation_sent",
+            "ID",
+            "Initials",
+            # "Gen",
+            # "Age",
+            # "CB_Tot_R",
+            # "SSC_Tot",
+            # "attachment_label",
+            # "behavior_problems_label",
+        ].copy
+    )
+    all_variables: bool = False
+    seed: int = 7
 
 
 if __name__ == "__main__":
@@ -49,12 +84,63 @@ if __name__ == "__main__":
         on="filename",
         how="outer",
     )
-    mst_transcripts = mst_transcripts.sort_values(by="filename")
+    mst_transcripts = (
+        mst_transcripts.sort_values(by="filename")
+        .replace(" ", pd.NA)
+        .dropna(subset=["ID"])
+    )
+    if not args.config.all_variables:
+        mst_transcripts = mst_transcripts[
+            args.config.columns + [args.config.target_variable]
+        ].dropna(subset=[args.config.target_variable])
+        logging.info(f"Initial shape of the dataset: {mst_transcripts.shape}")
 
-    logging.info(f"Final shape of the dataset: {mst_transcripts.shape}")
+        if args.config.target_variable_type == "categorical":
+            logging.info("Removing low frequency labels.")
+            mst_transcripts = mst_transcripts[
+                mst_transcripts.groupby(args.config.target_variable)[
+                    args.config.target_variable
+                ]
+                .transform("count")
+                .ge(args.config.min_label_frequency)
+            ]
+            logging.info(f"Shape of the dataset after removal: {mst_transcripts.shape}")
 
-    # Write output
-    if ".csv" in args.config.output_file:
-        mst_transcripts.to_csv(args.config.output_file, index=False)
-    elif ".xlsx" in args.config.output_file:
-        mst_transcripts.to_excel(args.config.output_file, index=False)
+            train, test, _, _ = train_test_split(
+                mst_transcripts,
+                mst_transcripts[args.config.target_variable],
+                test_size=args.config.test_size,
+                stratify=mst_transcripts[args.config.target_variable],
+                random_state=args.config.seed,
+            )
+        elif args.config.target_variable_type == "numeric":
+            train, test, _, _ = train_test_split(
+                mst_transcripts,
+                mst_transcripts[args.config.target_variable],
+                test_size=args.config.test_size,
+                stratify=mst_transcripts[args.config.target_variable],
+            )
+        else:
+            raise ValueError(
+                "Target variable type must be 'numeric' or 'categorical'."
+                f" Not '{args.config.target_variable_type}'"
+            )
+
+        output_path = os.path.join(
+            args.config.output_dir, f"{args.config.target_variable}_formatted"
+        )
+
+        logging.info(f"Train shape: {train.shape}")
+        logging.info(f"Test shape: {test.shape}")
+
+        train.to_csv(
+            f"{output_path}_train.csv",
+            index=False,
+        )
+        test.to_csv(
+            f"{output_path}_test.csv",
+            index=False,
+        )
+    else:
+        output_path = os.path.join(args.config.output_dir, f"mst_formatted")
+        mst_transcripts.to_csv(output_path, index=False)
