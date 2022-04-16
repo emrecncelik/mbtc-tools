66aef53d99e17112a39518f4ac3ef488ee811cd5
emrecncelik <emrecncelik@gmail.com>
2022-04-16 17:27:07 +0300

    update clf script

--- scripts/run_classification.sh
+++ scripts/run_classification.sh
@@ -2,7 +2,7 @@ INPUT_DIR="/home/emrecan/workspace/psychology-project/data"
 OUTPUT_DIR="/home/emrecan/workspace/psychology-project/outputs"
 CATEGORICAL_TARGETS=("attachment_label" "behavior_problems_label" "SSC_Tot" "Gen")
 # AVERAGING=("simple" "weighted_average" "weighted_removal")
-AVERAGING=("weighted_average")
+AVERAGING=("weighted_average" "simple")
 
 for target in "${CATEGORICAL_TARGETS[@]}"
 do
@@ -10,9 +10,12 @@ do
     do
         python classification.py \
             --input_dir "$INPUT_DIR" \
-            --output_dir "$OUTPUT_DIR" \
+            --output_dir "$OUTPUT_DIR/$averaging" \
             --seed 1 \
             --averaging "$averaging" \
-            --target_column "$target" 
+            --target_column "$target" \
+            --weight_per_entity 5 \
+            --min_weight 1 \
+            --do_test
     done
 done
